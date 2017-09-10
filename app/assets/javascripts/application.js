05d4eb6e984c6a316cdbe5d6a5eaa6940ed1aac2
spencerthomas <tom@cognitives.io>
2017-09-10 21:56:31 +1000

    gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery 3
+//= require popper
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
